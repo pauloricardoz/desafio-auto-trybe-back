13efa02dee2b60825f4234231a1b92dd57dac4fb
pauloricardoz <trybe.przt@gmail.com>
2022-01-23 01:21:08 -0300

    car model test checking object

--- tests/models.test.js
+++ tests/models.test.js
@@ -1,8 +1,12 @@
 const { expect } = require('chai');
 const carModel = require('../models/carModel');
+const connection = require('../models/connection');
 
 describe('car model', function () {
-  it('exist car model', function () {
-    expect(carModel).to.be('function');
+  it('exist car model', async function () {
+    expect(carModel).to.be.an('object');
+    expect(carModel).to.have.all.keys(['getAll'])
+    // const data = await connection('test');
+    // data.collection('colec1').insertOne({ name: 'Zambs' });
   });
 });
