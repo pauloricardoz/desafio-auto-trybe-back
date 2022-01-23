4962945f933b1ea4b83ad5ae574436a30f91dbe1
pauloricardoz <trybe.przt@gmail.com>
2022-01-23 01:22:57 -0300

    creating connection file

---
+++ models/connection.js
@@ -0,0 +1,12 @@
+const mongo = require('mongodb');
+
+let connection = null;
+
+const dbUrl = 'mongodb://localhost:27017';
+
+const getConnection = async (dbName) => {
+  connection = connection || await new mongo.MongoClient(dbUrl).connect();
+  return connection.db(dbName);
+};
+
+module.exports = getConnection;
