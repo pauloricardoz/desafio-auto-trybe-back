a92e2ec592336c0dba3ad43651714ed32892cf54
pauloricardoz <trybe.przt@gmail.com>
2022-01-23 21:17:17 -0300

    feat: car model implementation

--- models/carModel.js
+++ models/carModel.js
@@ -1,5 +1,25 @@
 const connection = require('./connection');
 
-const getAll = () => null;
+const dbName = 'carModel';
+const dbCollection = 'cars';
 
-module.exports = { getAll };
+const getAll = async () => connection.getConnection(dbName)
+  .then((db) => db.collection(dbCollection).find())
+  .then((result) => result.toArray());
+
+const insertCar = async (car) => 
+  connection.getConnection(dbName)
+    .then((db) => db.collection(dbCollection).insertOne(car));
+
+const editCar = async (car) => {
+  const { _id: id } = car;
+  return connection.getConnection(dbName)
+    .then((db) => db.collection(dbCollection).updateOne({ _id: id }, { $set: car }));
+};
+const deleteCar = async (id) => connection.getConnection(dbName)
+  .then((db) => db.collection(dbCollection).deleteOne({ _id: id }));
+const getByProperties = async (properties) => connection.getConnection(dbName)
+.then((db) => db.collection(dbCollection).find(properties))
+.then((result) => result.toArray());
+  
+module.exports = { getAll, insertCar, editCar, deleteCar, getByProperties };
