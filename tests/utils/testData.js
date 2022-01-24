db53eb4675aacb054730490cefe0f209584c5869
pauloricardoz <trybe.przt@gmail.com>
2022-01-24 02:56:01 -0300

    testData utils

---
+++ tests/utils/testData.js
@@ -0,0 +1,24 @@
+module.exports = {
+  newCar: {
+    Type: 'carro',
+    brand: 'fiat',
+    Model: 'palio',
+    Version: '1.0',
+    Year: 2012,
+    mileage: 10,
+    transmissionType: 'manual',
+    sellPrice: 20000,
+    dateReference: new Date(),
+  },
+  editedCar: {
+    Type: 'carro',
+    brand: 'fiat',
+    Model: 'palio',
+    Version: '1.3',
+    Year: 2012,
+    mileage: 10,
+    transmissionType: 'manual',
+    sellPrice: 20000,
+    dateReference: new Date(),
+  },
+};
