5ce9e56b55d1f480386d4de79c483f77f77122f9
pauloricardoz <trybe.przt@gmail.com>
2022-01-25 07:52:57 -0300

    feat: getByProperties service

--- services/carService.js
+++ services/carService.js
@@ -8,28 +8,10 @@ const editCar = async () => null;
  
 const deleteCar = async () => null;
 
-const isRightTypes = {
-  type: 'string',
-  brand: 'string',
-  model: 'string',
-  version: 'string',
-  year: 'number',
-  mileage: 'number',
-  transmissionType: 'string',
-  sellPrice: 'number',
-  dateReference: 'string',
-};
-
-const isValidVehicleProperties = (car) => !(car instanceof Object)
-  || !Object.keys(car).length
-  || !Object.keys(car)
-    .every((property) => typeof car[property] === isRightTypes[property])
-  || !Object.keys(car)
-    .every((property) => isRightTypes[property] === 'string' && car[property].length);
-
 const getByProperties = async (car) => {
-  if (isValidVehicleProperties(car)
-  ) return { isError: true };
+  if (isInvalidVehicleProperties(car)) {
+    return { isError: true };
+  }
   return carModel.getByProperties(car);
 };
 
