f9dd760e8786526340515682f5afbacb0d2af8c1
pauloricardoz <trybe.przt@gmail.com>
2022-01-26 06:25:54 -0300

    feat: creating app with express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const carRouter = require('./Routers/carRouters');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/car', carRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Auto-Trybe at port ${PORT}`));
