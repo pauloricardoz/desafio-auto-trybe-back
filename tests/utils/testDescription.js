b5a3b10135a4eb0dac16b02c67e1192882df1eb1
pauloricardoz <trybe.przt@gmail.com>
2022-01-26 06:14:13 -0300

    style: correction on lint

---
+++ tests/utils/testDescription.js
@@ -0,0 +1,12 @@
+module.exports = {
+  TypeWrongDataType: 'Type wrong data type', 
+  TypeRigthDataTypeButEmptyString: 'Type rigth data type, but empty string', 
+  brandWrongDataType: 'brand wrong data type', 
+  ModelWrongDataType: 'Model wrong data type', 
+  VersionWrongDataType: 'Version wrong data type', 
+  YearWrongDataType: 'Year wrong data type', 
+  mileageWrongDataType: 'mileage wrong data type',
+  transmissionTypeWrongDataType: 'transmissionType wrong data type', 
+  sellPriceWrongDataType: 'sellPrice wrong data type', 
+  dateReferenceWrongDataType: 'dateReference wrong data type', 
+};
